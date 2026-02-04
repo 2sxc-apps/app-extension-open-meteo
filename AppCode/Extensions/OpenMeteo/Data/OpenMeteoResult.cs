0bf7104fe42f7984e5e9ba854d390cdb079323e6
giovi <npro9021@gmail.com>
2026-02-04 08:42:41 +0100

    added Celsius -> Fahrenheit

---
+++ AppCode/Extensions/OpenMeteo/Data/OpenMeteoResult.cs
@@ -0,0 +1,10 @@
+namespace AppCode.Data
+{
+  public partial class OpenMeteoResult
+  {
+    /// <summary>
+    /// Temperature in Fahrenheit, calculated from the Celsius value.
+    /// </summary>
+    public decimal TemperatureFahrenheit => (Temperature * 9 / 5) + 32;
+  }
+}
